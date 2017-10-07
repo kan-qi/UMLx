8db141eb557c2464ffe475d745d605cb225de202
Shashank Shekhar <shashankeshav@gmail.com>
2017-10-07 15:25:32 -0700

    added UUID field, page redirected to home after submission

--- UMLxServices.js
+++ UMLxServices.js
@@ -52,7 +52,8 @@ app.post('/uploadUMLFile', upload.fields([{name:'uml-file',maxCount:1},{name:'um
 	var umlModelName = req.body['uml-model-name'];
 	var umlModelType = req.body['uml-model-type'];
 	var repoId = req.body['repo-id'];
-	return;
+	var uuidVal = req.body['uuid'];
+//	return;
 	umlModelInfoManager.queryRepoInfo(repoId, function(repoInfo){
 		var umlFileInfo = umlFileManager.getUMLFileInfo(repoInfo, umlFilePath, umlModelType, umlModelName);
 //		console.log('umlFileInfo');
