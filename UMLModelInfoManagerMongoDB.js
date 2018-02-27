868526afbe3c5f854e8c3a9612964453b3b6432d
AnushaBilakanti <anusha19rao@gmail.com>
2018-02-27 10:33:51 -0800

    Updated saveModelInfo

--- UMLModelInfoManagerMongoDB.js
+++ UMLModelInfoManagerMongoDB.js
@@ -545,43 +545,75 @@
 
     */
 
-    function saveModelInfo(modelInfo, useCaseInfo, domainModelInfo, modelId, useCaseId, domainModelId, callbackfunc) {
+   function saveModelInfo(modelInfo, repoId, callbackfunc) {
         MongoClient.connect(url, function(err, db) {
             if (err) throw err;
 //			console.log(modelInfo);
             var fs = require("fs");
             fs.writeFile('./temp/modelInfo1.json', JSON.stringify(modelInfo, null, 2) , 'utf-8');
             console.log(repoId);
-            var model_id = new mongo.ObjectID(modelId);
-			var UseCaseUUID= new mongo.ObjectID(useCaseId);
-			var domainModelUUID = new mongo.ObjectID(domainModelId);
-            db.collection("modelInfo").update({_id:model_id}, {$push: {Model_id: modelInfo[formInfo][_id]}}, function(err, res) {
-                if (err) throw err;
+			var o_id = new mongo.ObjectID(repoId);
+          
+			//retrieve use case id’s of modelInfo
+			var useCaseIdArray=new Array();
+			
+			for(var i in modelInfo.useCases){
+				var useCase = modelInfo.useCases[i];
+				useCaseIdArray.push(useCase._id);
+			}
+			
+			
+			
+			//retrieve domainModelId’s of modelInfo
+			var domainModelId = modelInfo.domainModel._id;
+			
+			
+			var useCases = modelInfo.useCases;
+			var domainModelInfo = modelInfo.domainModel;
+			
+			//removing usecase array
+			delete modelInfo.useCases;
+			//removing domainmodel
+			delete modelInfo.domainModel;
+			modelInfo.useCaseUUIDs = useCaseIdArray;
+			modelInfo.domainModelUUID = domainModelId;
+			
+//			var model_id = new mongo.ObjectID(modelId);
+//			var UseCaseUUID= new mongo.ObjectID(useCaseId);
+//			var domainModelUUID = new mongo.ObjectID(domainModelId);
+		
+			db.collection("repos").update({_id:o_id}, {$push: {modelUUIDs: new mongo.ObjectID(model_id)}}, function(err, res) {
+				if (err) throw err;
                 console.log("1 record inserted");
-                db.close();
-                if(callbackfunc !== null){
-                    callbackfunc(modelInfo);
-                }
-            });
-			db.collection("useCaseInfo").update({_id:UseCaseUUID}, {$push: {Models: useCaseInfo}}, function(err, res) {
-                if (err) throw err;
-                console.log("1 record inserted");
-                db.close();
-                if(callbackfunc !== null){
-                    callbackfunc(modelInfo);
-                }
-            });
-			db.collection("domainModelInfo").update({_id:domainModelUUID}, {$push: {Models: domainModelInfo}}, function(err, res) {
-                if (err) throw err;
-                console.log("1 record inserted");
-                db.close();
-                if(callbackfunc !== null){
-                    callbackfunc(modelInfo);
-                }
-            });
-        });
-    }
 
+                db.collection("modelInfo").insertOne(modelInfo, function(err, res) {
+                    if (err) throw err;
+                    console.log("1 record inserted");
+
+                
+                  //updating modelInfo with usecase id array and domain id
+        			
+                        db.collection("domainModelInfo").insertOne(domainModelInfo, function(err, res) {
+                            if (err) throw err;
+                            console.log("1 record inserted");
+                            
+                            db.collection("useCaseInfo").insertMany(useCases, function(err, res){
+                                if (err) throw err;
+                                console.log("1 record inserted");
+
+                            db.close();
+                            if(callbackfunc !== null){
+                                callbackfunc(modelInfo);
+                            }
+                        });
+                    }	);
+                });
+                
+                
+            });
+			
+        });
+}
 	//function updateRepoAnalytics(repoAnalytics, callbackfunc){
 		//MongoClient.connect(url, function(err, db) {
 			//if (err) throw err;
