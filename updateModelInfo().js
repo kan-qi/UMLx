23545a683e5804f38c24ecfa58273ad5d3bfa561
Vibhanshu Sharma <vibhanshu05@gmail.com>
2018-03-05 15:53:05 -0800

    updateModelInfo.js
    Tested Verified updateModelInfo function

--- updateModelInfo().js
+++ updateModelInfo().js
@@ -1,45 +1,44 @@
- function updateModelInfo(modelInfo, repoId, callbackfunc){
+  function updateModelInfo(modelInfo, repoId, callbackfunc){
         MongoClient.connect(url, function(err, db) {
             if (err) throw err;
             
-             var o_id = new mongo.ObjectID(modelInfo._id);
+            var o_id = new mongo.ObjectID(modelInfo._id);
             
-            //adding model_id in domainModelInfo
-            modelInfo.DomainModel.model_id = o_id;
-            
-            var domainObject = modelInfo.domainModel;
+            var domainObject = modelInfo.DomainModel;
             var useCaseArray = modelInfo.UseCases;
-            
+           
             //adding model_id in domainModelInfo
-            //domainObject.model_id = o_id;
+            domainObject.model_id = o_id;
             
             //removing domainModel and UseCase object from the model object 
-            delete modelInfo.domainModel;
+            delete modelInfo.DomainModel;
             delete modelInfo.UseCases;
             
-            for(var useCaseObject in useCaseArray){
+           /* for(var useCaseObject in useCaseArray){
                 useCaseObject.model_id = new mongo.ObjectID(modelInfo._id);   
-            }
+            }*/
             
             //adding rep_id as an element in modelInfo
             modelInfo.rep_id = repoId;
             
+             
             //updating domainModelInfo to add model_id element
             db.collection("domainModelInfo").update({_id: mongo.ObjectId(domainObject._id)}, domainObject, function(err, res) {
                 if (err) throw err;
                 console.log("updating domainModelInfo");
-                
-                //updating useCaseInfo to add model_id element
-                db.collection("useCaseInfo").update({model_id: mongo.ObjectId(modelInfo._id)}, useCaseObject, function(err, res) {
-                    if (err) throw err;
-                    console.log("updating domainModelInfo");
-           
-                });
+                         
             });
             
-            db.close();
+            //updating useCaseInfo to add model_id element
+                db.collection("useCaseInfo").update({}, {$set : {"model_id": o_id}}, function(err, res) {
+                    if (err) throw err;
+                    console.log("updating useCaseInfo");
+                    
+              });
+            
+             db.close();
                 if(callbackfunc !== null){
-                    callbackfunc("Success "+modelInfo);
+                    callbackfunc(modelInfo);
                 }
-            });         
+        });         
     }
