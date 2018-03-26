7e3ce999e9ac971c10bee92eaeef62fa6cc72fb1
Atharva Kale <atharvakale01@gmail.com>
2018-03-25 23:11:50 -0700

    Updated code to check for numeric columns

--- StatisticsOutput.R
+++ StatisticsOutput.R
@@ -1,46 +1,37 @@
 #!/usr/bin/env Rscript
 
-# # arg1: element statistics file path
-# # arg2: path staitstics file path
-# # arg3: expanded path statistics file path
-# # arg4: diagram statistics file path
-# # arg5: output directory
+# # arg1: input csv file path
+# # arg2: output directory
 library(e1071)
 library(jsonlite)
 
 args = commandArgs(trailingOnly=TRUE)
+newFile = args[1]
+outputDir <- args[2]
 
-elementStatisticsPath = args[1]
-pathStatisticsPath = args[2]
-expandedPathStatisticsPath = args[3]
-usecaseStatisticsPath = args[4]
-outputDir <- args[5]
-
-elementData <- read.csv(elementStatisticsPath, header=TRUE)
-pathData <- read.csv(pathStatisticsPath, header=TRUE)
-expandedPathData <- read.csv(expandedPathStatisticsPath, header=TRUE)
-usecaseData <- read.csv(usecaseStatisticsPath, header=TRUE)
-# columns names of integer datatype columns as (dataframe+$+column name) format
-column_names = c("usecaseData$average_degree","usecaseData$average_path_length"
-                 ,"usecaseData$architecture_difficulty","usecaseData$path_number",
-                 "pathData$path_length","pathData$avg_degree","pathData$arch_diff",
-                 "expandedPathData$path_length","elementData$outboundDegree",
-                 "elementData$inboundDegree")
-list_column_names = vector("list",length(column_names))
+filenameDataframe <- read.csv(newFile, header=TRUE)
+var1 <- sapply(filenameDataframe,is.numeric)
+var2 <- filenameDataframe[,var1]
+column_names_dataframe = vector("list")
+filename <- deparse(substitute(filenameDataframe))
+column_names <- colnames(var2)
 for(i in 1:length(column_names)){
-  column = eval(parse(text = column_names[i]))
+  column_names_dataframe[[i]] = (paste(filename,column_names[[i]],sep="$"))
+}
+json_output = vector("list",length(column_names_dataframe))
+for(i in 1:length(column_names_dataframe)){
+  column = eval(parse(text = column_names_dataframe[i]))
   mean_column = mean(as.numeric(column),na.rm = TRUE)
   var_column = var(as.numeric(column),na.rm = TRUE)
   quantile_column = quantile(as.numeric(column),na.rm = TRUE)
   kurtosis_column = kurtosis(as.numeric(column),na.rm = TRUE)
-  list_column_names[[i]] <- list("column name" = column_names[i],statistics = list(mean=mean_column,variance= var_column,
-                                                                   first_quartile=quantile_column[2],median=quantile_column[3],
-                                                                   third_quartile=quantile_column[4],kurtosis=kurtosis_column))
+  json_output[[i]] <- list("column name" = column_names[i],statistics = list(mean=mean_column,variance= var_column,
+                                                                             first_quartile=quantile_column[2],median=quantile_column[3],
+                                                                             third_quartile=quantile_column[4],kurtosis=kurtosis_column))
 }
 # print to output directory
 reportPath <- paste(outputDir,'statistics.json', sep='/')
 sink(reportPath)
 # write statistics in JSON format
-print(jsonlite::toJSON(list_column_names,pretty = TRUE,auto_unbox = TRUE))
+print(jsonlite::toJSON(json_output,pretty = TRUE,auto_unbox = TRUE))
 closeAllConnections()
-
