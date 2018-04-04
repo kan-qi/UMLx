837201be16cbbff86330493fbde2c6ee6430463e
DESKTOP-P0VKIRM\Anusha Bilakanti <anusha19rao@gmail.com>
2018-04-03 17:17:22 -0700

    calling saveModelInfo

--- UMLModelInfoManagerMongoDB.js
+++ UMLModelInfoManagerMongoDB.js
@@ -741,10 +741,10 @@ function queryRepoInfoByPage(repoId, stepParameter, pageParameter,callbackfunc)
             ] 
     }
 
-    /*saveModelInfo(modelInfo, "5a8e109c13a5974144158d99", function(result)
+    saveModelInfo(modelInfo, "5a8e109c13a5974144158d99", function(result)
     {
          console.log(result);
-    })*/
+    })
 
     function saveModelInfo(modelInfo, repoId, callbackfunc)
     {
