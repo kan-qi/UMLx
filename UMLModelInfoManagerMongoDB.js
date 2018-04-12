7fe557d08c3550f2da220a1b224050dd911c67a5
Vibhanshu Sharma <vibhanshu05@gmail.com>
2018-04-12 15:17:59 -0700

    Updated UMLModelInfoManagerMongoDB

--- UMLModelInfoManagerMongoDB.js
+++ UMLModelInfoManagerMongoDB.js
@@ -1486,6 +1486,7 @@ function deleteRepo(repoId, callbackfunc) {
 		updateUseCaseInfo: updateUseCaseInfo,
 		saveModelInfo : saveModelInfo,
 		queryRepoInfo : queryRepoInfo,
+		queryRepoInfoByPage:queryRepoInfoByPage,
 		queryUseCaseInfo: queryUseCaseInfo,
 		repoDetail:repoDetail,
 		saveEstimation: saveEstimation,
