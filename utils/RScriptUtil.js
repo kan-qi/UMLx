caa75b4124484a0d1b4222df3e7f46323eea9a23
flyqk <flyqk1991@gmail.com>
2018-04-14 01:25:14 -0700

    create pool mechanism for threads management when executing the R scripts

--- utils/RScriptUtil.js
+++ utils/RScriptUtil.js
@@ -5,23 +5,45 @@
 	
 	var RExec = '\"C:/Program Files/R/R-3.2.5/bin/Rscript\"';
 	
+	//every other functions which call R script should call this function.
+	
+	var commandPool = [];
+	var runningCount = 0;
+	var threadLimit = 10;
+	
 	function runRScript(command, callbackfunc){
 //		console.log('generate model Analytics');
 		console.log(command);
-		var child = exec(RExec+" "+command, function(error, stdout, stderr) {
-
-			if (error) {
-//				console.log('exec error: ' + error);
-				console.log(error);
-				if(callbackfunc){
-					callbackfunc(false);
-				}
-			} else {
-				if(callbackfunc){
-					callbackfunc(true);
-				}
-			}
+		commandPool.push({
+			command: command,
+			callbackfunc: callbackfunc
 		});
+		loop();
+	}
+	
+	function loop(){
+		if(runningCount < threadLimit){
+			var commandObject = commandPool.shift();
+			runningCount++;
+			var child = exec(RExec+" "+commandObject.command, function(error, stdout, stderr) {
+				if (error) {
+//					console.log('exec error: ' + error);
+					console.log(error);
+					if(commandObject.callbackfunc){
+						commandObject.callbackfunc(false);
+					}
+				} else {
+					if(commandObject.callbackfunc){
+						commandObject.callbackfunc(true);
+					}
+				}
+				
+				runningCount--;
+				if(commandPool.length > 0){
+					loop();
+				}
+			});
+		}
 	}
 	
 	module.exports = {
