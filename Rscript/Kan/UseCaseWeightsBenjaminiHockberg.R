75f0066ca93504b3eeaf1cda74a6bc2a487700cc
flyqk <kqi@usc.edu>
2018-07-15 00:44:41 -0700

    added more estimators into benjamini-hochberg testing

--- Rscript/Kan/UseCaseWeightsBenjaminiHockberg.R
+++ Rscript/Kan/UseCaseWeightsBenjaminiHockberg.R
@@ -46,9 +46,9 @@ accuracyD3 <- read.csv(dataUrl3, header=TRUE)
 
 dfs <- list(accuracyD1, accuracyD2, accuracyD3)
 
-t_tests <- as.data.frame(matrix(c(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0), nrow=3, ncol=4))
+t_tests <- as.data.frame(matrix(c(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0), nrow=3, ncol=12))
 rownames(t_tests) <- c("d1", "d2", "d3")
-colnames(t_tests) <- c("mmre", "pred15", "pred25", "pred50")
+colnames(t_tests) <- c("mmre_apriori", "pred15_apriori", "pred25_apriori", "pred50_apriori", "mmre_regression", "pred15_regression", "pred25_regression", "pred50_regression", "mmre_original", "pred15_original", "pred25_original", "pred50_original")
 p_value_list <- data.frame(t_test=character(), p_value=double())
 
 #p_value_list <- data.frame(t_test=c("mmre", "pred15", "pred25", "pred50", "mmre", "pred15", "pred25", "pred50", "mmre", "pred15", "pred25", "pred50"), p_value=c(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0))
@@ -66,18 +66,47 @@ for( i in 1:length(dfs)){
 	
 	#print(df)
 	
-	t_tests[row, "mmre"]<- (t.test(df$bayesian_mmre, df$apriori_mmre, var.equal=FALSE, paired=FALSE))[['p.value']]
+	#significance test on apriori
+	t_tests[row, "mmre_apriori"]<- (t.test(df$bayesian_mmre, df$apriori_mmre, var.equal=FALSE, paired=FALSE))[['p.value']]
 	#p_value_list[nrow(p_value_list)+1, ] = list(t_test=paste(row, "_mmre", sep=""), p_value=t_tests[row, "mmre"])
-	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_mmre", sep=""), p_value=t_tests[row, "mmre"]), stringsAsFactors=FALSE)
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_mmre_apriori", sep=""), p_value=t_tests[row, "mmre_apriori"]), stringsAsFactors=FALSE)
 	
-	t_tests[row, "pred15"] <- (t.test(df$bayesian_pred15, df$apriori_pred15, var.equal=FALSE, paired=FALSE))[['p.value']]
-	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred15", sep=""), p_value=t_tests[row, "pred15"]), stringsAsFactors=FALSE)
+	t_tests[row, "pred15_apriori"] <- (t.test(df$bayesian_pred15, df$apriori_pred15, var.equal=FALSE, paired=FALSE))[['p.value']]
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred15_apriori", sep=""), p_value=t_tests[row, "pred15_apriori"]), stringsAsFactors=FALSE)
 	
-	t_tests[row, "pred25"] <- (t.test(df$bayesian_pred25, df$apriori_pred25, var.equal=FALSE, paired=FALSE))[['p.value']]
-	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred25", sep=""), p_value=t_tests[row, "pred25"]), stringsAsFactors=FALSE)
+	t_tests[row, "pred25_apriori"] <- (t.test(df$bayesian_pred25, df$apriori_pred25, var.equal=FALSE, paired=FALSE))[['p.value']]
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred25_apriori", sep=""), p_value=t_tests[row, "pred25_apriori"]), stringsAsFactors=FALSE)
 	
-	t_tests[row, "pred50"] <- (t.test(df$bayesian_pred50, df$apriori_pred50, var.equal=FALSE, paired=FALSE))[['p.value']]
-	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred50", sep=""), p_value=t_tests[row, "pred50"]), stringsAsFactors=FALSE)
+	t_tests[row, "pred50_apriori"] <- (t.test(df$bayesian_pred50, df$apriori_pred50, var.equal=FALSE, paired=FALSE))[['p.value']]
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred50_apriori", sep=""), p_value=t_tests[row, "pred50_apriori"]), stringsAsFactors=FALSE)
+	
+	#significance test on regression
+	t_tests[row, "mmre_regression"]<- (t.test(df$bayesian_mmre, df$regression_mmre, var.equal=FALSE, paired=FALSE))[['p.value']]
+	#p_value_list[nrow(p_value_list)+1, ] = list(t_test=paste(row, "_mmre", sep=""), p_value=t_tests[row, "mmre"])
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_mmre_regression", sep=""), p_value=t_tests[row, "mmre_regression"]), stringsAsFactors=FALSE)
+	
+	t_tests[row, "pred15_regression"] <- (t.test(df$bayesian_pred15, df$regression_pred15, var.equal=FALSE, paired=FALSE))[['p.value']]
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred15_regression", sep=""), p_value=t_tests[row, "pred15_regression"]), stringsAsFactors=FALSE)
+	
+	t_tests[row, "pred25_regression"] <- (t.test(df$bayesian_pred25, df$regression_pred25, var.equal=FALSE, paired=FALSE))[['p.value']]
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred25_regression", sep=""), p_value=t_tests[row, "pred25_regression"]), stringsAsFactors=FALSE)
+	
+	t_tests[row, "pred50_regression"] <- (t.test(df$bayesian_pred50, df$regression_pred50, var.equal=FALSE, paired=FALSE))[['p.value']]
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred50_regression", sep=""), p_value=t_tests[row, "pred50_regression"]), stringsAsFactors=FALSE)
+	
+	#significance test on original
+	t_tests[row, "mmre_original"]<- (t.test(df$bayesian_mmre, df$original_mmre, var.equal=FALSE, paired=FALSE))[['p.value']]
+	#p_value_list[nrow(p_value_list)+1, ] = list(t_test=paste(row, "_mmre", sep=""), p_value=t_tests[row, "mmre"])
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_mmre_original", sep=""), p_value=t_tests[row, "mmre_original"]), stringsAsFactors=FALSE)
+	
+	t_tests[row, "pred15_original"] <- (t.test(df$bayesian_pred15, df$original_pred15, var.equal=FALSE, paired=FALSE))[['p.value']]
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred15_original", sep=""), p_value=t_tests[row, "pred15_original"]), stringsAsFactors=FALSE)
+	
+	t_tests[row, "pred25_original"] <- (t.test(df$bayesian_pred25, df$original_pred25, var.equal=FALSE, paired=FALSE))[['p.value']]
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred25_original", sep=""), p_value=t_tests[row, "pred25_original"]), stringsAsFactors=FALSE)
+	
+	t_tests[row, "pred50_original"] <- (t.test(df$bayesian_pred50, df$original_pred50, var.equal=FALSE, paired=FALSE))[['p.value']]
+	p_value_list = rbind(p_value_list, list(t_test=paste(row, "_pred50_original", sep=""), p_value=t_tests[row, "pred50_original"]), stringsAsFactors=FALSE)
 	
 	print(t_tests)
 
@@ -95,7 +124,7 @@ print("sorted p value list")
 print(sorted_p_value_list)
 
 # false discovery rate
-q <- 0.25
+q <- 0.30
 rank <- seq(1, nrow(sorted_p_value_list), by=1)
 
 print("rank")
