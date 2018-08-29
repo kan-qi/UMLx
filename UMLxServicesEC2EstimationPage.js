b4d86aa0a4308909a7fb31799128eefd01c5bd73
flyqk <kqi@usc.edu>
2018-08-29 00:52:52 -0700

    updated the services for the simplified page

--- UMLxServicesEC2EstimationPage.js
+++ UMLxServicesEC2EstimationPage.js
@@ -239,7 +239,7 @@ app.post('/predictProjectEffort', upload.fields([{name:'distributed_system',maxC
                 effortPredictor.predictEffortByModel(modelInfo, estimationModel, function(estimationResults){
                     if(!estimationResults){
                         console.log("error");
-                        res.render('estimationResultPane', {error: "inter process error"});
+                        res.render('estimationResultPaneSimplified', {error: "inter process error"});
                         return;
                     }
 
@@ -263,7 +263,7 @@ app.post('/predictProjectEffort', upload.fields([{name:'distributed_system',maxC
 //					console.log(modelInfo);
 
 
-                        res.render('estimationResultPane', {estimationResults:estimationResults, modelInfo: modelInfo});
+                        res.render('estimationResultPaneSimplified', {estimationResults:estimationResults, modelInfo: modelInfo});
 
 ////
                     });
