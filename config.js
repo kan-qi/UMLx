e4f9ecaae29ea693b16f76dfad55c87c91ab47f2
HaihaoLi93 <haihaoli@usc.edu>
2018-09-14 20:11:24 -0700

    issue#408

--- config.js
+++ config.js
@@ -7,6 +7,7 @@
 module.exports ={
 		'secret' :'projectumlx',
 		'secretUserInvite' :'umlxUserInvite',
+		'r libraries:' :'C:/Users/haiha/Documents/R/win-library/3.2;C:/Program Files/R/R-3.2.5/library'
 //		'debugOutputDir': debugOutputDir,
 //		setDebugOutputDir: setDebugOutputDir
 }
