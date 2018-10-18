cb2806cb934fea2e11ec7691f4bc18e60d154bbb
flyqk <flyqk1991@gmail.com>
2018-10-18 13:42:27 -0700

    local commit for merging remote branch

--- config.js
+++ config.js
@@ -7,15 +7,11 @@
 module.exports ={
 		'secret' :'projectumlx',
 		'secretUserInvite' :'umlxUserInvite',
-<<<<<<< HEAD
 		//'r libraries:' :'C:/Users/haiha/Documents/R/win-library/3.2;C:/Program Files/R/R-3.2.5/library'
 		"r_libraries": "C:/Users/flyqk/Documents/R/win-library/3.5;C:/Program Files/R/R-3.5.1/library",
 	     "eclipseInstall": "\"C:\\Users\\flyqk\\Documents\\Research Projects\\tools\\eclipse-java-luna-SR2-win32-x86_64\\eclipse\\eclipse\"",
 		 "defaultEclipseWorkSpace" : "C:\\Users\\flyqk\\workspace\\.metadata"
 //		"r_libraries": "C:\Users\haiha\Documents\R\win-library\3.2;C:\Program Files\R\R-3.2.5\library",
-=======
-		"r_libraries": "C:/Program Files/R/R-3.5.1/library",
->>>>>>> 336f1e484b60ac10f85c4835355ff5c517cd9328
 	     //"eclipseInstall": "\"C:\\Users\\flyqk\\Documents\\Research Projects\\tools\\eclipse-java-luna-SR2-win32-x86_64\\eclipse\\eclipse\"",
 		 //"defaultEclipseWorkSpace" : "C:\\Users\\flyqk\\workspace\\.metadata"
 //		'debugOutputDir': debugOutputDir,
