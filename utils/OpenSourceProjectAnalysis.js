b15e5bf1107769a9788bb2fa905d06257346d77f
flyqk <flyqk1991@gmail.com>
2018-11-23 07:21:30 -0800

    comment out the database save system

--- utils/OpenSourceProjectAnalysis.js
+++ utils/OpenSourceProjectAnalysis.js
@@ -165,7 +165,8 @@ function analyseXMIModel(projectList, reportDir){
 
 function scanRepo(repoListPath, repoRecordPath){
 		  //to generate svg file.
-			var classPath = '"C:\\Users\\flyqk\\Documents\\Research Projects\\UMLx\\facility-tools\\Repo Analyser\\bin"';
+//			var classPath = '"C:\\Users\\flyqk\\Documents\\Research Projects\\UMLx\\facility-tools\\Repo Analyser\\bin"';
+			var classPath = '".\\facility-tools\\Repo Analyser\\bin"';
 		    var command = 'java -classpath '+classPath+' repo.AnalysisKit "scan-repo" "'+repoListPath+'" "'+repoRecordPath+'"';
 		  	console.log(command);
 		  	var child = exec(command, {maxBuffer: 1024 * 1024*100, stdio: 'ignore' }, function(error, stdout, stderr) {
@@ -184,7 +185,8 @@ function scanRepo(repoListPath, repoRecordPath){
 
 function selectFiles(repoRecordPath){
 	 //to generate svg file.
-	var classPath = '"C:\\Users\\flyqk\\Documents\\Research Projects\\UMLx\\facility-tools\\Repo Analyser\\bin"';
+//	var classPath = '"C:\\Users\\flyqk\\Documents\\Research Projects\\UMLx\\facility-tools\\Repo Analyser\\bin"';
+	var classPath = '".\\facility-tools\\Repo Analyser\\bin"';
     var command = 'java -classpath '+classPath+' repo.AnalysisKit "select-files" "'+repoRecordPath+'"';
   	console.log(command);
   	var child = exec(command, {maxBuffer: 1024 * 1024*100, stdio: 'ignore' }, function(error, stdout, stderr) {
@@ -202,7 +204,8 @@ function selectFiles(repoRecordPath){
 
 function analyseSloc(repoRecordPath){
 	 //to generate svg file.
-	var classPath = '"C:\\Users\\flyqk\\Documents\\Research Projects\\UMLx\\facility-tools\\Repo Analyser\\bin"';
+//	var classPath = '"C:\\Users\\flyqk\\Documents\\Research Projects\\UMLx\\facility-tools\\Repo Analyser\\bin"';
+	var classPath = '".\\facility-tools\\Repo Analyser\\bin"';
    var command = 'java -classpath '+classPath+' repo.AnalysisKit "analyse-sloc" "'+repoRecordPath+'"';
  	console.log(command);
  	var child = exec(command, {maxBuffer: 1024 * 1024*100, stdio: 'ignore' }, function(error, stdout, stderr) {
@@ -220,7 +223,8 @@ function analyseSloc(repoRecordPath){
 
 function generateSlocReport(repoRecordPath){
 	 //to generate svg file.
-	var classPath = '"C:\\Users\\flyqk\\Documents\\Research Projects\\UMLx\\facility-tools\\Repo Analyser\\bin"';
+//	var classPath = '"C:\\Users\\flyqk\\Documents\\Research Projects\\UMLx\\facility-tools\\Repo Analyser\\bin"';
+	var classPath = '".\\facility-tools\\Repo Analyser\\bin"';
   var command = 'java -classpath '+classPath+' repo.AnalysisKit "generate-report" "'+repoRecordPath+'"';
 	console.log(command);
 	var child = exec(command, {maxBuffer: 1024 * 1024*100, stdio: 'ignore' }, function(error, stdout, stderr) {
