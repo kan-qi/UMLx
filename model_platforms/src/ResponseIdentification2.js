18e8374da7e8eb24f08ade74cd0c34ee151c4736
flyqk <kqi@usc.edu>
2018-12-13 13:24:56 -0800

    in the progress of implementing advanced call graph and the reponse identification

---
+++ model_platforms/src/ResponseIdentification2.js
@@ -0,0 +1,144 @@
+/**
+ * This module is used to parse src code into USIM model. The construction is currently based on KDM. Further implementation can be made by using AST, which needs further investigation.
+ * 
+ * This script relies on KDM and Java model 
+ * 
+ * The goal is the establish the control flow between the modules...
+ * Identify the stimuli.
+ * Identify the boundary.
+ * Identify the sytem components.....
+ */
+(function() {
+	var fs = require('fs');
+	var xml2js = require('xml2js');
+	var parser = new xml2js.Parser();
+	var jsonQuery = require('json-query');
+	var jp = require('jsonpath');
+	var codeAnalysis = require("./CodeAnalysis.js");
+
+//	var xpath = require('xpath');
+//	var dom = require('xmldom').DOMParser;
+	
+	function identifyResponse(codeAnalysisResults, responsePatternsFilePath){
+		var dicMethodUnits = codeAnalysisResults.dicMethodUnits;
+		var methodPatterns = ['main'];
+		var parameterTypePatterns = ['event', 'Event'];
+		var parameterPatterns = ['event', 'Event'];
+		
+		var scannedMethods = [];
+		
+		if (responsePatternsFilePath && fs.existsSync(responsePatternsFilePath)) {
+		 
+		var contents = fs.readFileSync(responsePatternsFilePath, 'utf8');
+		
+//		console.log(contents);
+		
+		if(contents){
+		var lines = contents.split(/\r?\n/g);
+		 
+	    for(var i = 0;i < lines.length;i++){
+	        //code here using lines[i] which will give you each line
+	    	var line = lines[i];
+	    	
+	    	var segs = line.split(/ /g);
+	    	
+	    	if(segs.length == 2){
+	    		var type = segs[0];
+	    		if(type === "method"){
+	    			methodPatterns.push(segs[1]);
+	    		}
+	    		else if(type === "param"){
+	    			parameterPatterns.push(segs[1]);
+	    		}
+	    		else if(type === "param-type"){
+	    			parameterTypePatterns.push(segs[1]);
+	    		}
+	    	}
+	    }
+		}
+		   // Do something
+		}
+		
+//		console.log("method patterns");
+//		console.log(methodPatterns);
+//		console.log("parameter type patterns");
+//		console.log(parameterTypePatterns);
+//		console.log("parameter patterns");
+//		console.log(parameterPatterns);
+		
+		var dicResponseMethodUnits = {};
+		
+		for(var i in dicMethodUnits){
+			var methodUnit = dicMethodUnits[i];
+//			
+//
+//			if(methodUnit.name === "main"){
+//				MethodUnit.isResponse = true;
+//			}
+//			
+			for(var j in methodPatterns){
+				if(methodUnit.Signature.name.match(methodPatterns[j])){
+					methodUnit.isResponse = true;
+					break;
+				}	
+			}
+			
+			if(methodUnit.isResponse){
+				dicResponseMethodUnits[methodUnit.UUID] = methodUnit;
+				continue;
+			}
+			
+			for(var j in methodUnit.parameterUnits){
+				var parameterUnit = methodUnit.parameterUnits[j];
+//				if(parameterUnit.type.indexOf("event") !=-1 || parameterUnit.type.indexOf("Event") !=-1) {
+//						MethodUnit.isResponse = true;
+//						console.log("found response method");
+//				}
+				
+				for(var k in parameterPatterns){
+					var parameterPattern = parameterPatterns[k];
+					if(parameterUnit.name.match(parameterPattern)){
+						methodUnit.isResponse = true
+						break;
+					}
+				}
+				
+				if(methodUnit.isResponse){
+					dicResponseMethodUnits[methodUnit.UUID] = methodUnit;
+					break;
+				}
+				
+				for(var k in parameterTypePatterns){
+					var parameterTypePattern = parameterTypePatterns[k];
+					if(parameterUnit.type.name.match(parameterTypePattern)){
+						methodUnit.isResponse = true
+						break;
+					}
+				}
+				
+
+				if(methodUnit.isResponse){
+					dicResponseMethodUnits[methodUnit.UUID] = methodUnit;
+					break;
+				}
+			}
+		}
+		
+		
+//		for(var i in dicMethodUnits){
+//			var methodUnit = dicMethodUnits[i];
+//			scannedMethods.push({method: methodUnit.Signature.name, isResponse: methodUnit.isResponse});
+//			
+//		}
+		
+//		var debug = require("../../utils/DebuggerOutput.js");
+//		debug.writeJson("identifiedMethods", scannedMethods);
+		
+		return dicResponseMethodUnits;
+	}
+		
+	
+	module.exports = {
+			identifyResponse : identifyResponse,
+	}
+}());
