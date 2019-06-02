170de9894d49319634ba2f499b316de11395e3d8
Hsuan-Hau Liu <hsuanhal@usc.edu>
2019-06-01 21:04:45 -0700

    refactored and fixed returning weird emails

--- data/GitAndroidAnalysis/repo_analysis/email-collection.R
+++ data/GitAndroidAnalysis/repo_analysis/email-collection.R
@@ -14,115 +14,181 @@ library(stringr)
 
 
 # Variables
-# set user-agent
-user = "flyqk"
-password = "qk@github/910304"
 input_filename = "repos.xlsx"
 output_filename = "emails.xlsx"
+MAX_NUM_EMAILS = 500 # set a maximum number of emails to retrieve for each project
 
-# import datasheet as DataFrame
-data = read.xlsx(input_filename, colNames=TRUE)
-output <- data.frame()
+# set user-agent
+agent_user = "flyqk"
+agent_password = "qk@github/910304"
+authentication = authenticate(agent_user, agent_password)
 
 
-# local functions
-getTotalPages <- function(linkHeaderStr) {
-  matched <- stringr::str_match(linkHeaderStr, "page=(\\d+)>; rel=\\\"last\\\"")
-   return(as.integer((matched[, 2])))
-}
-
-rbind.match.columns <- function(input1, input2) {
-  n.input1 <- ncol(input1)
-  n.input2 <- ncol(input2)
+getTotalPages <- function(info) {
+  # Finds the last page from the API GET request.
+  #
+  # Args:
+  #   info: content from API GET request -> http response object
+  #
+  # Returns:
+  #   The last page number; the number of pages in the request -> integer
   
-  if (n.input2 < n.input1) {
-    TF.names <- which(names(input2) %in% names(input1))
-    column.names <- names(input2[, TF.names])
-  } else {
-    TF.names <- which(names(input1) %in% names(input2))
-    column.names <- names(input1[, TF.names])
+  linkHeaderStr = info$headers$link
+  totalPages = 1
+  if (!is.null(linkHeaderStr)) {
+    last_page <- stringr::str_match(linkHeaderStr, "page=(\\d+)>; rel=\\\"last\\\"")
+    totalPages = as.integer((last_page[, 2]))
   }
-  
-  return(rbind(input1[, column.names], input2[, column.names]))
+  return(as.integer(totalPages))
 }
 
-# Convert repo URL to API URL
+
 convertToAPI <- function(repo_url) {
-  API_path = "https://api.github.com/repos"
+  # Converts Github repo URL to API URL.
+  #
+  # Args:
+  #   repo_url: Github repository URL -> characters
+  #
+  # Returns:
+  #   The converted URL -> characters
+  
+  api_path = "https://api.github.com/repos"
   extracted_repo_name = unlist(strsplit(as.character(repo_url), "https://github.com/"))[2]
-  return(paste(API_path, extracted_repo_name, sep="/", collapse=NULL))
-}
-
-# Get user e-mail
-getEmail <- function(user_info) {
-  email = stringr::str_split(user_info, "email", 2)[[1]][2]
-  email = stringr::str_split(email, "name", 2)[[1]][1]
-  email = stringr::str_split(email, "\": \"", 2)[[1]][2]
-  return(stringr::str_split(email, "\",\n", 2)[[1]][1])
+  return(paste(api_path, extracted_repo_name, sep="/", collapse=NULL))
 }
 
 
-# read email segment and return data
-for(i in 1:nrow(data)) {
-  repo_name = data[i, 1]
-  repo_url = data[i, 2]
-  contributors <- list()    # list of contributors
+getEmail <- function(user_name, user_info) {
+  # Gets user's e-mail from user's recent event API page.
+  #
+  # Args:
+  #   user_name: user's Github name -> characters
+  #   user_info: GET request from user's recent event page -> http response object
+  #
+  # Returns:
+  #   Retrieved email -> characters/NA
+  
+  user_info_json = content(user_info, as="parsed")
+  len = length(user_info_json)
+  if (len > 0) {
+    for (i in 1:len) {
+      # check if payload and commit tags exist
+      commits = user_info_json[[i]]$payload$commits
+      if (!is.null(commits) && length(commits)) {
+        user_email = commits[[1]]$author$email
+        
+        # check if this email belongs to the user
+        if (!is.null(user_email) && user_name == user_info_json[[i]]$actor$login) {
+          return(user_email)
+        }
+      }
+    }
+  }
+  return(NA)
+}
+
+
+getContributors <- function(repo_name, repo_url) {
+  # Gets all contributors of the repository
+  #
+  # Args:
+  #   user_name: user's Github name -> characters
+  #   user_info: GET request from user's recent event page -> http response object
+  #
+  # Returns:
+  #   Retrieved email -> characters/NA
   
   print(paste("## ", repo_name, " ##"))
+  contributors <- list()
   
   # find contriutor list by retrieving info from the contributor page
-  url = convertToAPI(repo_url)
-  contrib_page = paste(url, "/contributors?page=1", sep='')
-  info <- GET(contrib_page, authenticate(user, password))
-  pages <- getTotalPages(info$headers$link)
+  api_url = convertToAPI(repo_url)
+  contrib_page = paste(api_url, "/contributors?page=1", sep='')
+  info <- GET(contrib_page, authentication)
+  totalPages <- getTotalPages(info)
   
-  if (length(pages) > 0) {
-    print(paste(i, ": if"))
+  # convert JSON to DataFrame
+  currentPage <- fromJSON(content(info, "text"), flatten=TRUE)
+  contributors[[1]] <- currentPage
+  
+  if (totalPages > 1) {
+    print(paste("Found", totalPages, "pages."))
     
-    for (k in seq(1:pages)) {
-      contrib_page = paste(url, "/contributors?page=", k, sep='')
-      info <- GET(contrib_page, authenticate(user, password))
-      currentPage <- fromJSON(content(info, "text"), flatten = TRUE)
-      currentPage$repo_name <- repo_name
-      contributors[[k]] <- currentPage
+    for (page in 2:totalPages) {
+      contrib_page = paste(api_url, "/contributors?page=", page, sep='')
+      info <- GET(contrib_page, authentication)
+      currentPage <- fromJSON(content(info, "text"), flatten=TRUE)
+      contributors[[page]] <- currentPage
     }
   } else {
-    print(paste(i, ": else"))
-    
-    contrib_page = paste(url, "/contributors", sep='')
-    info <- GET(contrib_page, authenticate(user, password))
-    currentPage <- fromJSON(content(info, "text"), flatten = TRUE)
-    currentPage$repo_name <- repo_name
-    contributors[[i]] <- currentPage
+    print(paste("Only 1 page was found."))
   }
+  all_contributors = do.call("rbind", contributors) # combine all DataFrame into one
+  all_contributors$repo_name <- repo_name
+  return(all_contributors)
+}
+
+
+getEmails <- function(contributors) {
+  # Retrieve emails of each contributor.
+  #
+  # Args:
+  #   contributors: all contributors of the repo -> DataFrame
+  #
+  # Returns:
+  #   all contributors with their emails and contributions -> DataFrame
   
-  contributors <- rbind_pages(contributors)
-  contributors_info = contributors[, 1]
-  
-  # find user info
-  max = 200
-  line <- data.frame(name=c(), email=c(), contributions = c(), repo_name=c())
-  for (k in 1:nrow(contributors[1])) {
-    user_url = paste("https://api.github.com/users", contributors[k, 1], "events/public", sep="/")
-    print(paste("User URL:", user_url))
+  email_table <- data.frame(name=c(), email=c(), contributions = c(), repo_name=c())
+  counter = 0
+  for (user in 1:nrow(contributors)) {
+    user_name = contributors[user, 1]
+    user_url = paste("https://api.github.com/users", user_name, "events/public", sep="/")
+    print(paste("Fetching user", user_name, "email from:", user_url))
     
-    user_info <- GET(user_url, authenticate(user, password))
-    user_info = as.character(user_info)
-    email = getEmail(user_info)
-    name = as.character(contributors[[1]][k])
+    user_info <- GET(user_url, authentication)
+    user_email = getEmail(user_name, user_info)
     
-    if (is.na(email) == FALSE) {
-      sub_line <- data.frame(name = name, email = email,
-                             contributions = contributors[["contributions"]][k],
-                             repo_name = contributors[["repo_name"]][k])
-      line <- rbind(line, sub_line)
-      email = NA
-      print(sub_line)
+    if (!is.na(user_email)) {
+      counter = counter + 1
+      email <- data.frame(name = user_name, email = user_email,
+                          contributions = contributors[["contributions"]][user],
+                          repo_name = contributors[["repo_name"]][user])
+      email_table <- rbind(email_table, email)
+      print("Added new row:")
+      print(email)
+      if (counter == MAX_NUM_EMAILS) {
+        break
+      }
+    }
+    else {
+      print("Cannot find email.")
     }
   }
   
-  # append our result to the output
-  output <- rbind(output, line)
+  print(paste("Retrieved", counter, "out of", nrow(contributors), "emails."))
+  return(email_table)
 }
 
-write.xlsx(output, output_filename)
+
+run <- function(input_filename, output_filename) {
+  # Retrieve emails from contributors of each project in the input file and output the results.
+  #
+  # Args:
+  #   input_filename: name of the input xlsx file -> characters
+  #   output_filename: name of the output xlsx file -> characters
+  
+  input_data = read.xlsx(input_filename, colNames=TRUE) # import datasheet as DataFrame
+  output <- data.frame() # output DataFrame
+  
+  for (project in 1:nrow(input_data)) {
+    contributors <- getContributors(input_data[project, 1], input_data[project, 2])
+    output <- rbind(output, getEmails(contributors)) # append result to the output DataFrame
+  }
+  
+  # output to xlsx file
+  write.xlsx(output, output_filename)
+  print(paste("Finished running. Output has been stored in the", output_filename, "file."))
+}
+
+
+run(input_filename, output_filename)
