e97f37d2ecdb951c720907b1aa44e3f0c39930a6
GloriaW1126 <zicaiwan@usc.edu>
2020-07-29 19:57:38 -0700

    Update stepwise_linear_model.R

--- data/Benchmark/stepwise_linear_model.R
+++ data/Benchmark/stepwise_linear_model.R
@@ -3,32 +3,23 @@ m_fit.step_lnr <- function(step_lnr,dataset){
   #step_lnr <- models$step_lnr
   #dataset = modelData
   
-  cols = step_lnr$regression_cols
-  #for(i in 1:length(cols)){
-  #  print(cols[i])
-  #  print(dataset[, cols[i]])
-  #}
-  dataset <- dataset[, cols]
   
   cleanData <- clean_step(dataset[, colnames(dataset) != "Effort"])
-  #str_frm <- paste("Effort ~",step_lnr$formula)
   dims <- colnames(cleanData)
-  print(dims)
   step_lnr$dims <- dims
   str_frm <- paste("Effort ~", paste(dims, collapse="+"))
-  #print(str_frm)
+  frm <- as.formula(str_frm)
   regressionData <- cleanData
   regressionData$Effort <- dataset$Effort
-  frm <- as.formula(str_frm)
-  #step_m <- lm(frm, data=dataset)
   step_m <- lm(frm, data=regressionData)
   step_lnr$m <- step_m
   
+  library(MASS)
   tryCatch( { step_lnr$m <- stepAIC(step_m, direction = "both", trace = FALSE) }
             , error = function(e) {
               print("-infinite AIC")
-              })
- 
+            })
+  
   #step_lnr$cols_removed = c()
   #print(step_lnr$m)
   step_lnr
@@ -36,10 +27,8 @@ m_fit.step_lnr <- function(step_lnr,dataset){
 
 m_predict.step_lnr <- function(step_lnr, testData){
   # numeric data only
-  testData <- testData[, step_lnr$dims]
-  numeric_columns <- unlist(lapply(testData, is.numeric))
-  data.numeric <- testData[, numeric_columns]
-  data.numeric <- data.frame(apply(testData, 2, as.numeric))
+  testData<-dplyr::select_if(testData, is.numeric)
+  
   #cols_removed = step_lnr$cols_removed
   predicted <- predict(step_lnr$m, testData)
 }
@@ -67,70 +56,59 @@ stepwise_linear_model <- function(modelData, regression_cols=c()){
 
   # Preprocess dataset
 clean_step <- function(dataset){
-  #print("clean....")
-  #print(rownames(dataset))
   # numeric data only
-  numeric_columns <- unlist(lapply(dataset, is.numeric))
-  data.numeric <- dataset[, numeric_columns]
-  data.numeric <- data.frame(apply(dataset, 2, as.numeric))
+  data.numeric<-dplyr::select_if(dataset, is.numeric)
   
   ## Impute
   library(mice)
   library(randomForest)
-  # perform mice imputation, based on random forests.
-  # print(md.pattern(data))
-  
-  data.imputed = data.numeric
+  # perform mice imputation, based on rf.
+  data.imputed <- data.numeric
   tryCatch( { 
-       miceMod <- mice(data.numeric, method="rf", print=FALSE, remove_collinear = TRUE)
-       # generate the completed data.
-       data.imputed <- mice::complete(miceMod)
-    }, error = function(e) {
-          print("nothing left to impute")
+    miceMod <- mice(data.numeric, method="rf", print=F, remove_collinear = F)
+    # generate the completed data.
+    data.imputed <- mice::complete(miceMod)
+  }, error = function(e) {
+    print("nothing left to impute")
   })
+  
   
   # remove near zero variance columns
   library(caret)
-  nzv_cols <- nearZeroVar(data.imputed)
+  nzv_cols <- nearZeroVar(data.numeric)
   if(length(nzv_cols) > 0) data <- data.imputed[, -nzv_cols]
-  
-  #sapply(data, function(x) sum(is.na(x)))
-  
+
   if(is.null(data) || is.null(dim(data)) || dim(data)[2] < 2){
-    data = data.imputed
+    data <- data.imputed
   }
   
-  # remove collinear columns
-  #need to consider the number of columns after colinearity analysis to make sure that the column number is fewer than row number.
+  #special case for D3: small sample size
+  while(dim(data)[1]<=10){
+    data<-rbind(data,data)
+  }
   
-  descrCorr <- cor(data)
+  #reduce number of predictors to avoid "inifite -AIC" problem
+  #start from highly correlated predictors, till the ratio between variables and data is 2
+  descrCorr <- cor(data,use='pairwise.complete.obs')
+  #print(descrCorr)
   data.done = data
-  #cor_limits <- c(0.99, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1)
-  cor_limits <- seq(0.99, 0.01, by = -0.02)
+  cor_limits <- c(0.99, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1)
   for(i in 1:length(cor_limits)){
     highCorr <- findCorrelation(descrCorr, cor_limits[i])
-    
     data.removed <- data[, -highCorr]
-    #coli <- findLinearCombos(data.imputed1)
-    
-    #print("data frame removed: ")
-    #print(highCorr)
-    #print(c(ncol = ncol(data.removed), nrow = nrow(data.removed)))
     
     if(is.null(data.removed) || is.null(dim(data.removed)) || dim(data.removed)[2] < 2){
+      print("nothing left below correlation coeefition=",cor_limits[i] )
       break
     }
     
-    #ste the ratio between variables and data to avoid "inifite -AIC" proglem.
+    #set the ratio between variables and data to avoid "inifite -AIC" problem.
     if(nrow(data.removed) > ncol(data.removed)*2){
-      data.done = data.removed
+      data.done <- data.removed
+      cat("avoid inifite at cor=",cor_limits[i])
       break
     }
   }
-  
-  #coli <- findLinearCombos(data.imputed)
-  #print(coli)
-  #data.done <- data.imputed[, -coli$remove]
-  
   return(data.done)
+  #}
 }
