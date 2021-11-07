63bed72b542fdd09f6c12ade3aeb0ec132747d27
flyqk <flyqk1991@gmail.com>
2021-11-07 10:59:21 -0800

    commit local changes

---
+++ facility-tools/Android-toolkit/src/Configs.java
@@ -0,0 +1,43 @@
+import java.util.List;
+
+public class Configs {
+    public static String outputDir = "./output";
+    public static String benchmarkName = "annotatePod";
+    public static String project;
+    public static String sdkDir;
+    private static String appPkg = "de.danoeh.antennapod";
+
+    public static List<String> libraryPackages = null;
+
+    public static boolean isLibraryClass(String className) {
+        if (libraryPackages == null || libraryPackages.isEmpty()) {
+            if(className.startsWith(appPkg)){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        if(className.startsWith("java")) {
+            return true;
+        }
+        for (String pkg : libraryPackages) {
+            if (pkg.equals(className) ||
+                    ((pkg.endsWith(".*") || pkg.endsWith("$*")) &&
+                            className.startsWith(pkg.substring(0, pkg.length() - 1)))
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isGeneratedClass(String name) {
+        // TODO Auto-generated method stub
+        if(name.startsWith("androidx") || name.startsWith("android")) {
+            return true;
+        }
+
+        return false;
+    }
+}
